084f055ed1c45018c618ab2a50b42f59e53d347d
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2018-05-25 19:29:45 -0300

    Added remove event functionality

--- SocketIoClient.h
+++ SocketIoClient.h
@@ -37,6 +37,7 @@ public:
 	void loop();
 	void on(const char* event, std::function<void (const char * payload, size_t length)>);
 	void emit(const char* event, const char * payload = NULL);
+	void remove(const char* event);
 	void disconnect();
 };
 
