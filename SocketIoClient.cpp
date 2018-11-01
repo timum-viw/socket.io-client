568af4ed64d6a4a2a5e39125bc69a04dca77da76
jslay88 <justin.slay@gmail.com>
2018-10-31 23:24:35 -0600

    Add internal hexdump method for ESP32 support

--- SocketIoClient.cpp
+++ SocketIoClient.cpp
@@ -15,6 +15,16 @@ const String getEventPayload(const String msg) {
 	return result;
 }
 
+static void hexdump(const uint32_t* src, size_t count) {
+    for (size_t i = 0; i < count; ++i) {
+        Serial.printf("%08x ", *src);
+        ++src;
+        if ((i + 1) % 4 == 0) {
+            Serial.printf("\n");
+        }
+    }
+}
+
 void SocketIoClient::webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 	String msg;
 	switch(type) {
@@ -38,7 +48,7 @@ void SocketIoClient::webSocketEvent(WStype_t type, uint8_t * payload, size_t len
 			break;
 		case WStype_BIN:
 			SOCKETIOCLIENT_DEBUG("[SIoC] get binary length: %u\n", length);
-			hexdump(payload, length);
+			hexdump((uint32_t*) payload, length);
 		break;
 	}
 }
